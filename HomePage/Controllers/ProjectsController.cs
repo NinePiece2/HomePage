49c2f4c647d2340b81eef987e67ac690b4682dc3
NinePiece2 <romit.sagu@hotmail.com>
2024-02-29 20:10:10 -0500

    Add project files.

---
+++ HomePage/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomePage.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HomeServer()
+        {
+            return View();
+        }
+        
+        public IActionResult FaceGen()
+        {
+            return View();
+        }
+    }
+}
