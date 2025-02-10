e3811ac379f7b5a4bc0360763184db8bbd6a5872
NinePiece2 <romit.sagu@hotmail.com>
2025-02-10 13:45:07 -0500

    Add MRI Brain Tumor Detection

--- HomePage/Controllers/ProjectsController.cs
+++ HomePage/Controllers/ProjectsController.cs
@@ -200,5 +200,38 @@ namespace HomePage.Controllers
 
             return View(model);
         }
+
+        public async Task<IActionResult> MRIBrainTumorDetection()
+        {
+            var projectInfo = homePageContext.Applications.Where(x => x.Name == "MRI Brain Tumor Detection").FirstOrDefault();
+
+            string markdownContent = string.Empty;
+
+            // Check if the URL is not null or empty
+            if (!string.IsNullOrEmpty(projectInfo.GitHubReadMeLink))
+            {
+                markdownContent = await client.GetStringAsync(projectInfo.GitHubReadMeLink);
+
+                markdownContent = markdownContent.Replace("images/", projectInfo.GitHubReadMeImagesLink);
+            }
+            else
+            {
+                // Handle the case where the URL is missing
+                markdownContent = "No README URL found.";
+                throw new Exception(markdownContent);
+            }
+
+            var mkd = Markdown.ToHtml(markdownContent);
+
+            var model = new ProjectsViewModel
+            {
+                ProjectName = projectInfo.Name,
+                ProjectApplicationLink = projectInfo.ApplicationLink,
+                ProjectGithubLink = projectInfo.GitHubLink,
+                ProjectReadmeContent = mkd
+            };
+
+            return View(model);
+        }
     }
 }
