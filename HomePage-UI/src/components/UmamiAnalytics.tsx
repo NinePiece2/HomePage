78e5dace23700a56632bfafef37f3225611b9c78
Romit Sagu <romit.sagu@hotmail.com>
2025-10-12 04:16:44 -0400

    Fix umami analytics issue

--- HomePage-UI/src/components/UmamiAnalytics.tsx
+++ HomePage-UI/src/components/UmamiAnalytics.tsx
@@ -8,8 +8,8 @@ interface UmamiAnalyticsProps {
 }
 
 export default function UmamiAnalytics({
-  websiteId = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
-  src = process.env.NEXT_PUBLIC_UMAMI_URL,
+  websiteId = "6ece8859-1100-434a-8b17-a7183239ac63",
+  src = "https://analytics.romitsagu.com/script.js",
 }: UmamiAnalyticsProps) {
   if (!src || !websiteId) return null;
 
